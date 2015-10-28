7eeecfed85cff634b9e3fa55c8df13efecfc5ea4
b123400 <b123400@gmail.com>
2015-10-29 03:28:37 +0800

    finally get twitter auth working!

--- main.hs
+++ main.hs
@@ -2,18 +2,64 @@
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-import           Yesod
 
-data Home = Home
+import Data.Text as T hiding (map)
+import Data.ByteString (ByteString)
+import Yesod
+import Yesod.Auth
+import Yesod.Auth.OAuth
+import Network.HTTP.Client.Conduit (Manager, newManager)
 
-mkYesod "Home" [parseRoutes|
-/ HomeR GET
+data MyApp = MyApp
+    { httpManager :: Manager
+    }
+
+instance Yesod MyApp where
+    approot = ApprootStatic "http://localhost:3000"
+
+mkYesod "MyApp" [parseRoutes|
+/     HomeR GET
+/auth AuthR Auth getAuth
 |]
 
-instance Yesod Home
+instance RenderMessage MyApp FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+instance YesodAuth MyApp where
+    type AuthId MyApp = T.Text
+    authenticate = return . Authenticated . credsToText
+
+    loginDest _ = HomeR
+    logoutDest _ = HomeR
+    authPlugins _ = [authTwitter clientKey clientSecret]
+    maybeAuthId = lookupSession "_ID"
+    authHttpManager = httpManager
+
+clientKey :: ByteString
+clientKey = "l2GlANAaQcWk8EcwgFKDeRIsy"
+
+clientSecret :: ByteString
+clientSecret = "WtYgEtHkmkvrD1g69OXfbcTnRPgrJ6p8yK31NcbhjoXCD0Kq7m"
+
+credsToText :: Creds MyApp -> T.Text
+credsToText cred = T.intercalate "/" $ map (\(a, b)-> T.concat [a,":",b]) $ credsExtra cred
+
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = do
+    maid <- maybeAuthId
+    defaultLayout
+        [whamlet|
+            <p>Your current auth ID: #{show maid}
+            $maybe _ <- maid
+                <p>
+                    <a href=@{AuthR LogoutR}>Logout
+            $nothing
+                <p>
+                    <a href=@{AuthR LoginR}>Go to the login page
+        |]
+
 
 main :: IO ()
-main = warp 3000 Home
+main = newManager >>= \manager ->
+    warp 3000 $ MyApp manager
