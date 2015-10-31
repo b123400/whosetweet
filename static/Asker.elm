cc764605ed57b2f739d33cce0f9d32001343d006
b123400 <b123400@gmail.com>
2015-11-01 02:40:38 +0800

    show answer

--- static/Asker.elm
+++ static/Asker.elm
@@ -1,15 +1,23 @@
 module Asker (Action, Model, init, update, view) where
 
-import Html exposing (Html, div, button, text, a)
+import Html exposing (Html, Attribute, div, button, text, a)
 import Effects exposing (Effects)
 import TwitterTypes exposing (Tweet, User, Answer, Question)
-import Array
+import Array exposing (Array)
+import Signal
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 
 type Model = Asking Question
            | Answered Answer
 
 type Action = ShowQuestion Question
             | ShowAnswer Answer
+            | Reply User
+
+type ChoiceState = Correct
+                 | Wrong
+                 | NotSelected
 
 init : Question -> (Model, Effects Action)
 init question =
@@ -20,26 +28,62 @@ view address model =
     case model of
 
         Asking question ->
-            div [] ([ (text question.tweet.text) ] ++
-                    ( question.choices
-                    |> Array.toList
-                    |> List.map (.screenName) 
-                    |> List.map (\screenName ->
-                        div [] [ text screenName ]))
-                   )
+            div [] ( [(text question.tweet.text) ] ++
+                      (buttons question.choices (choiceAttributes address))
+            )
 
         Answered answer ->
-            div [] [ (text answer.question.tweet.text)
-                   , (text answer.answer.screenName)
-                   ]
+            div [] ([(text answer.question.tweet.text)] ++
+                     (buttons answer.question.choices
+                        (answerAttributes answer.answer answer.question.tweet.user)))
 
+
+buttons : Array User -> (User -> List Attribute) -> List Html
+buttons choices getAttribute =
+    choices
+    |> Array.map (\user ->
+        button (getAttribute user) [ text user.screenName ])
+    |> Array.toList
+
+choiceAttributes : Signal.Address Action -> User -> List Attribute
+choiceAttributes address user =
+    [ onClick address (Reply user) ]
+    -- Add some style here
+
+answerAttributes : User -> User -> User -> List Attribute
+answerAttributes answered correct current =
+    let isSelected = current.screenName == answered.screenName
+        isCorrect  = current.screenName == correct.screenName
+    in [ answerStyleAttributes (getChoiceState isSelected isCorrect) ]
+
+answerStyleAttributes : ChoiceState -> Attribute
+answerStyleAttributes state =
+    style [ (
+        case state of
+            Correct -> ("color", "green")
+            Wrong -> ("color", "red")
+            NotSelected -> ("","")
+    )]
+
+getChoiceState : Bool -> Bool -> ChoiceState
+getChoiceState selected correct =
+    case (selected, correct) of
+        (False, _)    -> NotSelected
+        (_, True)     -> Correct
+        (True, False) -> Wrong
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
-    case action of
+    case (action, model) of
 
-        ShowQuestion question ->
+        (ShowQuestion question, _) ->
             (Asking question, Effects.none)
 
-        ShowAnswer answer ->
+        (ShowAnswer answer, _) ->
             (Answered answer, Effects.none)
+
+        (Reply user, Asking question) ->
+            (Answered (Answer question user), Effects.none)
+
+        otherwise ->
+            (model, Effects.none)
